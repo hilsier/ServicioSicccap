c46ce8183487cdaade139b900b8e920a307ab6ca
hilsier <hilsier4simpleplan@gmail.com>
2014-11-05 12:43:26 -0600

    JuanHipsterAyotzinapo

--- src/java/WebService/FileAux.java
+++ src/java/WebService/FileAux.java
@@ -227,5 +227,28 @@ String pathAbsolute=ip+":8080/ServicioSicccap"+array[1];
 return pathAbsolute;
 
     }
+private boolean exists(String URLName){
+    try {
+      HttpURLConnection.setFollowRedirects(false);
+      // note : you may also need
+      //        HttpURLConnection.setInstanceFollowRedirects(false)
+      HttpURLConnection con =
+         (HttpURLConnection) new URL(URLName).openConnection();
+      con.setRequestMethod("HEAD");
+      return (con.getResponseCode() == HttpURLConnection.HTTP_OK);
+    }
+    catch (Exception e) {
+       e.printStackTrace();
+       return false;
+    }
+  }
+
+
+
+
+
+
+
+
 
 }//fin de la calse
