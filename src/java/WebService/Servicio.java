eb805f2387b157b6d7879528557036082f0804e4
hilsier <hilsier4simpleplan@gmail.com>
2014-11-13 15:22:34 -0600

    Extension

--- src/java/WebService/Servicio.java
+++ src/java/WebService/Servicio.java
@@ -121,7 +121,7 @@ System.out.println("log created:"+log.CreateLogSigned(WidthImage,HeigthImage,Num
             
             if(dirImage.isDirectory()&&dirImage.canRead()&&dirImage.canWrite()){
                 System.out.println("directory is valid");
-             archivoBMP = new File(FileAux.ImageFolder,FileName);
+             archivoBMP = new File(FileAux.ImageFolder,FileName+".png");
              img = new PNG(archivoBMP);
             
             }else{
